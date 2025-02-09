126287a10a960bdf7843d43e70310208fb59f562
Anton Zelenov <antonz@rolos.com>
2025-02-09 17:31:02 +0100

    Redo tests for metadata

--- tests/source/test_metadata.py
+++ tests/source/test_metadata.py
@@ -4,20 +4,20 @@ from source.metadata import ImageMetadata
 
 
 def test_get_description_not_present(test_image: Path) -> None:
-    result = ImageMetadata(test_image).get_description()
-    assert result is None
+    with ImageMetadata(test_image) as meta:
+        assert meta.get_description() is None
 
 
 def test_get_description_exif(test_image_with_exif_description: Path) -> None:
-    result = ImageMetadata(test_image_with_exif_description).get_description()
-    assert result == "Test description exif"
+    with ImageMetadata(test_image_with_exif_description) as meta:
+        assert meta.get_description() == "Test description exif"
 
 
 def test_get_description_iptc(test_image_with_iptc_description: Path) -> None:
-    result = ImageMetadata(test_image_with_iptc_description).get_description()
-    assert result == "Test description iptc"
+    with ImageMetadata(test_image_with_iptc_description) as meta:
+        assert meta.get_description() == "Test description iptc"
 
 
 def test_get_description_xmp(test_image_with_xmp_description: Path) -> None:
-    result = ImageMetadata(test_image_with_xmp_description).get_description()
-    assert result == 'lang="x-default" Test description xmp'
+    with ImageMetadata(test_image_with_xmp_description) as meta:
+        assert meta.get_description() == 'lang="x-default" Test description xmp'
